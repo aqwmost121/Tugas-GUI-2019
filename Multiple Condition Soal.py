853cd4da0d53573a46c5900a3f3f249ab1d2a5a3
Wildan Eri Saputra <43690512+aqwmost121@users.noreply.github.com>
2019-04-02 19:56:29 +0700

    Tugas Multiple Condition

---
+++ Multiple Condition Soal.py
@@ -0,0 +1,15 @@
+// Multiple Condition
+nlai = float(input("Masukkan Nilai : "))
+if nlai > 80:
+        print("A")
+elif nlai < 80 and nlai >=70 :
+        print("B")
+elif nlai < 70 and nlai >=60:
+        print("C")
+elif nlai < 60 and nlai >=50:
+        print("D")
+elif nlai < 50 and nlai >=40:
+        print("E")
+else:
+        print("Nilai Tidak Valid")
+enter 2x
